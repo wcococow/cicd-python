32decc9868ae27b45c0a76cd136d11ba1895463a
Kate Lee <wcococow@gmail.com>
2024-10-22 20:03:27 +0530

    5 commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from my CI/CD powered Flask app! V1'
+    return 'Hello from my CI/CD powered Flask app! V2'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
